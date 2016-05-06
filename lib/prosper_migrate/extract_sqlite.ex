8eb30eae1431b84f406d1425ce20f3c8b9286de9
Depierre Thomas <depierre.thomas@gmail.com>
2016-05-06 13:14:16 +0200

    Let's try again without shenanigans

--- lib/prosper_migrate/extract_sqlite.ex
+++ lib/prosper_migrate/extract_sqlite.ex
@@ -4,32 +4,17 @@ defmodule ProsperMigrate.ExtractSqlite do
 
   def extract_itemID() do
 
-    get_typeid_list([], 0, false)
+    get_typeid_list()
     |>Enum.sort()
   end
 
-  defp get_typeid_list(state, _position, true)do
-    state
-  end
-  defp get_typeid_list(state, position, false) do
-    query_result = query_1000(position)
-    continue = Enum.empty?(query_result)
+  defp get_typeid_list() do
+      query = from(s in "snapshot_evecentral")
+      |> distinct(true)
+      |> select([s], s."typeid")
 
-    new_state = if continue do
-                  Enum.uniq(query_result ++ state)
-                else
-                  state
-                end
-    get_typeid_list(new_state, position+10_000, continue)
-  end
-
-
-  defp query_1000(position) do
-    from(s in "snapshot_evecentral")
-    |> select([s], s."typeid")
-    |> limit(10_000)
-    |> offset(^position)
-    |> Repo.all()
+      query
+      |> Repo.all()
   end
 
   # def extract_item(itemID) do
@@ -42,15 +27,15 @@ defmodule ProsperMigrate.ExtractSqlite do
   #   |> Repo.all
   # end
 
-  def seed_influx() do
-    item_list=extract_itemID()
+#  def seed_influx() do
+#    item_list=extract_itemID()
 
 
     # item_list
     # |> Stream.map(&extract_item/1)
     # |> Stream.map(&seed_from_item/1)
     # |> Stream.run()
-  end
+#  end
 
   def seed_from_item(list) do
     list
